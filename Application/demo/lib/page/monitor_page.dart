0d45649d5e5161a460bbd5d052fce3c11f2b10c8
thetjswo <tjswodi96@gmail.com>
2024-02-29 22:35:28 +0900

    Feat: Monitor 화면 UI 제작
    - 카메라 이름 추가
    - 영상 화면 공간 추가
    - 영상 하단 텍스트 추가
    - 신고, 해제, 공유 버튼 추가

--- Application/demo/lib/page/monitor_page.dart
+++ Application/demo/lib/page/monitor_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class MonitorPage extends StatefulWidget {
   @override
@@ -7,12 +6,98 @@ class MonitorPage extends StatefulWidget {
 }
 
 class _MonitorPageState extends State<MonitorPage> {
-  String server_url = dotenv.get('SERVER_URL');
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(server_url),
+      child: Column(
+        children: [
+          SizedBox(height: 100,),
+          Container(
+            child: Column(
+              children: [
+                Container(
+                  height: 40,
+                  width: 350,
+                  padding: EdgeInsets.only(left: 10, top: 5),
+                  color: Color(0xFFD0A9F5),
+                  child: Text(
+                    'Camera01',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 197,
+                  width: 350,
+                  color: Colors.grey,
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 10, right: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      //TODO: 'n분 전' 동적으로 가져오는 기능 구현
+                      Text('2분 전 카메라 화면'),
+                      Text(
+                        '움직임 감지',
+                        style: TextStyle(
+                          color: Colors.blue
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 50,),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        child: Column(
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.emergency_outlined, color: Colors.red,),
+                              iconSize: 60,
+                            ),
+                            Text('신고하기')
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.check_circle_outline_outlined, color: Colors.green,),
+                              iconSize: 60,
+                            ),
+                            Text('경보해제')
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.share_outlined, color: Colors.yellow,),
+                              iconSize: 60,
+                            ),
+                            Text('공유하기')
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      )
     );
   }
 }
