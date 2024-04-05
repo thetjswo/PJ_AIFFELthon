af1c1cd6f836387c74cb4427afb05fc8ea3c9da0
Yanghee Claire Kim <145723730+claireyy01@users.noreply.github.com>
2024-04-05 23:06:12 +0900

    Feat: 장치관리 페이지 내 감지 시간 설정 기능 추가
    전반적인 화면 구성 작업을 진행했습니다.
    - 감지 시간 : 5, 10, 15, 20초 중에서 선택 가능. (서버로 전송하는 부분은 구현 예정)
    - 연결된 기기 이름 : 코드 상 'Camera01'로 표기된 부분. (서버에서 받아오는 부분은 구현 예정)

--- Application/cozy_house_client_dev/lib/page/device_management_page.dart
+++ Application/cozy_house_client_dev/lib/page/device_management_page.dart
@@ -1,4 +1,48 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+const List<String> list = <String>['5초', '10초', '15초', '20초'];
+
+class DetectTimes extends StatefulWidget {
+  const DetectTimes({super.key});
+
+  // DropdownButton의 상태를 생성하는 메서드
+  @override
+  State<DetectTimes> createState() => _DropdownMenuExampleState();
+}
+
+// DropdownButton의 상태를 관리하는 클래스
+class _DropdownMenuExampleState extends State<DetectTimes> {
+  String dropdownValue = list.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_drop_down),
+      elevation: 16,
+      style: const TextStyle(color: Colors.black, fontSize: 18),
+      // underline: Container(
+      //   height: 2,
+      //   color: Colors.deepPurpleAccent,
+      // ),
+      onChanged: (String? value) {
+        // This is called when the user selects an item.
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
+      items: list.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
+
+
 
 // TODO : 장치 관리 기능 구현 필요
 class DeviceManagementPage extends StatelessWidget {
@@ -16,8 +60,7 @@ class DeviceManagementPage extends StatelessWidget {
         ),
         centerTitle: true,
         leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back,
+          icon: const Icon(Icons.arrow_back,
             color: Color(0xFFA1DEFF),
           ),
           onPressed: () {
@@ -25,12 +68,53 @@ class DeviceManagementPage extends StatelessWidget {
           },
         ),
       ),
-      body: const Center(
-        child: Text(
-          '장치 관리 페이지 내용',
-          style: TextStyle(fontSize: 24),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Camera01',  // TODO: 기기 이름 불러오기
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                  fontSize: 18,
+                  height: 1.2,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '감지 시간 설정',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black
+                  ),
+                ),
+                DetectTimes(),
+              ],
+            ),
+            Text(
+              '외부인이 몇 초 이상 머물렀을 때 푸쉬 알림을 받을지 시간을 선택하세요.',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                  fontSize: 14,
+                  height: 1.5,
+                  color: Colors.grey
+              ),
+            ),
+          ],
         ),
-      ),
+      )
     );
   }
 }
+
