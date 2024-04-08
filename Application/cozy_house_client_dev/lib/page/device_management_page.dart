7c3998d400523e79c92f79f6c9b9dc0af9ee1f09
seulwithlove <love.and.seul@gmail.com>
2024-04-08 23:40:39 +0900

    Design: 카메라 이름 가운데 정렬 및 기타 디자인 수정
    - 패딩 top 추가

--- Application/cozy_house_client_dev/lib/page/device_management_page.dart
+++ Application/cozy_house_client_dev/lib/page/device_management_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 const List<String> list = <String>['20초', '15초', '10초', '5초'];
@@ -42,79 +41,69 @@ class _DropdownMenuExampleState extends State<DetectTimes> {
   }
 }
 
-
-
 // TODO : 장치 관리 기능 구현 필요
 class DeviceManagementPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          '장치 관리',
-          style: TextStyle(
+        appBar: AppBar(
+          title: const Text(
+            '장치 관리',
+            style: TextStyle(
+                color: Color(0xFFA1DEFF),
+                fontSize: 25,
+                fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
               color: Color(0xFFA1DEFF),
-              fontSize: 25,
-              fontWeight: FontWeight.bold
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
           ),
         ),
-        centerTitle: true,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back,
-            color: Color(0xFFA1DEFF),
-          ),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-        child: const Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Camera01',  // TODO: 기기 이름 불러오기
-              textAlign: TextAlign.start,
-              style: TextStyle(
-                  fontSize: 18,
-                  height: 1.2,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey
+        body: const Padding(
+          padding: EdgeInsets.fromLTRB(20, 30, 20, 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Camera01', // TODO: 기기 이름 불러오기
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 18,
+                    height: 1.2,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey),
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  '감지 시간 설정',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '감지 시간 설정',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
                   ),
-                ),
-                DetectTimes(),
-              ],
-            ),
-            Text(
-              '외부인이 몇 초 이상 머물렀을 때 푸쉬 알림을 받을지 시간을 선택하세요.',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                  fontSize: 14,
-                  height: 1.5,
-                  color: Colors.grey
+                  DetectTimes(),
+                ],
               ),
-            ),
-          ],
-        ),
-      )
-    );
+              Text(
+                '외부인이 몇 초 이상 머물렀을 때 푸시 알림을 받을지 시간을 선택하세요.',
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 14, height: 1.5, color: Colors.grey),
+              ),
+            ],
+          ),
+        ));
   }
 }
-
